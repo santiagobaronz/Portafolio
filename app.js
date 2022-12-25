f1a791c40575c31dea7935f3d2b3cb095506aae6
Santiago Baron Zuleta <santiagobazul@hotmail.com>
2022-12-24 20:09:39 -0500

    [Servidor] Se agrega servidor NodeJS - ExpressJS

---
+++ app.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const { default: fetch } = require('node-fetch');
+const mysql = require('mysql');
+require('dotenv').config({path:'./.env'})
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static('./'));
+app.use(express.json());
+
+/***************************************************************
+ *                     Database connection 
+***************************************************************/
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "santiagobaron"
+})
+
+connection.connect(error => {
+    if(error) throw error;
+    console.log('Base de datos conectada')
+})
+
+/***************************************************************
+ *                          Routes 
+***************************************************************/
+var publicPath = path.resolve(__dirname, 'public');
+app.use(express.static(publicPath));
+
+app.get('/', (req, res) => {
+    res.setHeader('Content-type', 'text/html');
+    res.sendFile('index.html', { root: './public/' })
+})
+
+/***************************************************************
+ *                          Server
+***************************************************************/
+
+const port = process.env.port || 2020;
+app.listen(port, () => console.log(`Escuchando en el puerto ${port}...`));
