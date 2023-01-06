101abacb578e0fc5b5336d109d64b3ed4de49311
Santiago Baron <santiagobazul@hotmail.com>
2023-01-05 22:17:41 -0600

    [Ajustes] Correciones varias

--- public/js/modules/fillProjects.js
+++ public/js/modules/fillProjects.js
@@ -60,7 +60,7 @@ export const fillProjects = async () => {
                 objectList.innerHTML = `
 
                         <div class="w-1/4 flex items-center">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="fill-white" viewBox="0 0 16 16">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="fill-black-300" viewBox="0 0 16 16">
                                 <path d="M8.186 1.113a.5.5 0 0 0-.372 0L1.846 3.5l2.404.961L10.404 2l-2.218-.887zm3.564 1.426L5.596 5 8 5.961 14.154 3.5l-2.404-.961zm3.25 1.7-6.5 2.6v7.922l6.5-2.6V4.24zM7.5 14.762V6.838L1 4.239v7.923l6.5 2.6zM7.443.184a1.5 1.5 0 0 1 1.114 0l7.129 2.852A.5.5 0 0 1 16 3.5v8.662a1 1 0 0 1-.629.928l-7.185 2.874a.5.5 0 0 1-.372 0L.63 13.09a1 1 0 0 1-.63-.928V3.5a.5.5 0 0 1 .314-.464L7.443.184z"/>
                               </svg>
                             <p class="font-normal text-md text-white ml-4">${repo.name}</p>
@@ -85,7 +85,7 @@ export const fillProjects = async () => {
                             </a>
                           </div>
 
-                          <div class="w-16 flex items-center">
+                          <div class="w-24 flex items-center">
                             <a href="${repo.svn_url}" target="blank">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="fill-black-500" viewBox="0 0 16 16">
                                     <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z"/>
@@ -94,7 +94,7 @@ export const fillProjects = async () => {
                         </div>
 
                         <div class="flex items-center justify-end">
-                          <span class="w-full bg-[#32d49e43] rounded-lg px-7 text-center py-3 text-[#32d49e] text-sm font-semibold deployed">Desplegado</span>
+                          <span class="w-full bg-[#32d49e43] rounded-lg px-7 text-center py-3 text-[#32d49e] text-sm font-semibold deployed">Activo</span>
                         </div>
 
                 `
